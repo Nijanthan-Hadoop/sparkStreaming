40008ae1ac46546635f9fec80d69123d4b831c19
nijanthan26 <bk820484895>
2018-02-17 13:39:03 +0530

    Add code

--- src/main/scala/streaming.scala
+++ src/main/scala/streaming.scala
@@ -1,4 +1,25 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SQLContext
+
 
 object streaming {
+  def main(args:Array[String]): Unit =
+  {
+    val conf = new SparkConf()
+      .setMaster("local[2]")
+      .setAppName("")
+    val sc = new SparkContext(conf)
+    val sqlContext = new SQLContext(sc)
+    import sqlContext.implicits._
 
+    val a=List(1,2,3,4,5)
+    for ( i <- a)
+    {
+      println(i)
+    }
+    val dfy=sc.parallelize(a).toDF
+    dfy.show()
+    println("a")
+  }
 }
