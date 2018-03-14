e7ec94cf500165d4a3a0da719461b98da0b2e8bd
nijanthan26 <bk820484895>
2018-03-14 18:38:57 +0530

    Add code

--- src/main/scala/kafkaProducer.scala
+++ src/main/scala/kafkaProducer.scala
@@ -8,7 +8,7 @@ import org.apache.avro.generic.GenericRecord
 import org.apache.avro.specific.SpecificDatumWriter
 import java.io.ByteArrayOutputStream
 import org.apache.avro.io._
-import kafka.producer.{KeyedMessage, Producer, ProducerConfig}
+//import kafka.producer.{KeyedMessage, Producer, ProducerConfig}
 import scala.io.Source
 
 object kafkaProducer {
