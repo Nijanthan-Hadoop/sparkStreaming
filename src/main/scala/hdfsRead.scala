9c5980f0036762788bd8acb22d2f18aa32f2ef9e
nijanthan26 <bk820484895>
2018-03-14 20:32:19 +0530

    Add code

--- src/main/scala/hdfsRead.scala
+++ src/main/scala/hdfsRead.scala
@@ -1,6 +1,9 @@
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.FileSystem
 import org.apache.hadoop.fs.Path
+import java.util.Properties
+import org.apache.kafka.clients.producer._
+import org.apache.parquet
 import java.io.PrintWriter
 
 
@@ -13,8 +16,24 @@ object hdfsRead {
     val fileSystem = FileSystem.get(conf)
     val stream = fileSystem.open(path)
     def readLines = Stream.cons(stream.readLine, Stream.continually( stream.readLine))
-
     readLines.takeWhile(_ != null).foreach(line => println(line))
+
+
+    val  props = new Properties()
+    props.put("bootstrap.servers", "localhost:9092")
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+
+    val producer = new KafkaProducer[String,String](props)
+    val TOPIC = "parquetRead"
+
+    for ( i <- 1 to 10)
+    {
+      val record = new ProducerRecord(TOPIC,"key",s"hello $i)
+        producer.send(record)
+    }
+
+    producer.close()
   }
 
 }
