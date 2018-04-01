65815ff163a959e6e9597498e335d59668f01d29
nijanthan26 <bk820484895>
2018-04-01 07:51:36 +0530

    Add code

--- build.sbt
+++ build.sbt
@@ -4,10 +4,10 @@ version := "0.1"
 
 scalaVersion := "2.11.11"
 
-assemblyMergeStrategy in assembly := {
-  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
-  case x => MergeStrategy.first
-}
+//assemblyMergeStrategy in assembly := {
+//  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+//  case x => MergeStrategy.first
+//}
 
 resolvers ++= Seq(
   Classpaths.typesafeReleases,
@@ -15,14 +15,15 @@ resolvers ++= Seq(
 )
 
 libraryDependencies += "org.apache.hadoop" % "hadoop-hdfs" % "2.6.0"
-libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.0"
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "1.6.0"
-libraryDependencies += "org.apache.spark" %% "spark-hive" % "1.6.0"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.2.0"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.0"
+libraryDependencies += "org.apache.spark" %% "spark-hive" % "2.2.0"
 libraryDependencies += "org.apache.parquet" % "parquet-format" % "2.4.0"
 libraryDependencies += "org.apache.parquet" % "parquet-avro" % "1.9.0"
 libraryDependencies += "org.apache.kafka" %% "kafka" % "0.11.0.2"
 libraryDependencies += "com.databricks" %% "spark-avro" % "4.0.0"
 libraryDependencies += "org.apache.avro" % "avro" % "1.8.2"
 libraryDependencies += "io.confluent" % "kafka-avro-serializer" % "3.2.1"
-libraryDependencies += "org.apache.spark" %% "spark-streaming" % "1.6.0" % "provided"
-libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka-0-10" % "1.6.0"
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.2.0" % "provided"
+libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka-0-10" % "2.2.0"
+//libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka" % "1.6.2"
---
+++ src/main/scala/oldConsumer.scala
@@ -0,0 +1,60 @@
+import org.apache.spark.rdd.RDD
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.avro.generic.GenericData.Record
+import org.apache.spark.sql.{ Encoders, SparkSession }
+import org.apache.spark.streaming.{ StreamingContext, _ }
+import io.confluent.kafka.serializers.KafkaAvroDeserializer
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.{ CanCommitOffsets, HasOffsetRanges, KafkaUtils, OffsetRange }
+
+object oldConsumer extends App {
+
+  val conf = new SparkConf().setMaster("local[2]").setAppName("kafka-consumer")
+  val sc = new SparkContext(conf)
+  val ssc = new StreamingContext(sc, Seconds(5))
+
+  val kafkaParams = Map[String, Object](
+    "bootstrap.servers" -> "localhost:9092",
+    "key.deserializer" -> "io.confluent.kafka.serializers.KafkaAvroDeserializer",
+    "value.deserializer" -> "io.confluent.kafka.serializers.KafkaAvroDeserializer",
+    "schema.registry.url" -> "http://0.0.0.0:8081",
+    "auto.offset.reset" -> "smallest",
+    "enable.auto.commit" -> (false: java.lang.Boolean),
+    "group.id" -> "group1")
+
+  val topics = Array("nijavro")
+  val stream = KafkaUtils.createDirectStream[String, String](ssc, PreferConsistent, Subscribe[String, String](topics, kafkaParams))
+  stream.print()
+
+  //ConsumerRecord(topic = test_mysql_sample, partition = 0, offset = 67, CreateTime = 1505379645403, checksum = 2903150596, serialized key size = -1, serialized value size = 19, key = null, value = {"c1":1,"c2":{"string":"a"},"c3":{"string":"b"}})
+
+  var offsetRanges = Array[OffsetRange]()
+
+  stream.transform {
+    rdd =>
+      //Cast the rdd to an interface that lets us get an array of OffsetRange
+      offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+      rdd
+  }.foreachRDD(rdd => {
+    if (rdd.count != 0) {
+      val hiveContext = new org.apache.spark.sql.hive.HiveContext(sc)
+      val rdds: RDD[String] = rdd.map(e => e.value().asInstanceOf[org.apache.avro.generic.GenericData.Record].toString)
+      val data = hiveContext.read.json(rdds)
+      data.printSchema()
+      data.show()
+//      data.registerTempTable("sourceData")
+//      hiveContext.sql("INSERT INTO TABLE default.kafka_demo_1 SELECT * FROM sourceData")
+
+      // some time later, after outputs have completed
+      stream.asInstanceOf[CanCommitOffsets].commitAsync(offsetRanges)
+
+    } else {
+      println("RDD is Empty - No data")
+    }
+  })
+
+  ssc.start()
+  ssc.awaitTermination()
+}
